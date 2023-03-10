9ac787166f7c72ecc677e9fd4277fdaac61298ee
hcarter1112 <hcarter1112@gmail.com>
2023-03-10 16:25:02 -0500

    test script

---
+++ git-setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+project="dotfiles.git"
+githubdir="hcarter1112"
+echo "-----------------------------------------------------------------------------"
+echo "this is project https://github.com/$githubdir/$project"
+echo "-----------------------------------------------------------------------------"
+
+git config --global pull.rebase false
+git config --global push.default simple
+git config --global user.name "hcarter1112"
+git config --global user.email "hcarter1112@gmail.com"
+sudo git config --system core.editor vim
+#git config --global credential.helper cache
+#git config --global credential.helper 'cache --timeout=32000'
+git remote set-url origin git@github.com:hcarter1112/dotfiles.git
+echo
+echo "Everything set"
